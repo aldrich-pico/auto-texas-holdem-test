152a32224af2297ded2e104244b114255c510df5
aldrich-pico <62074615+aldrich-pico@users.noreply.github.com>
2021-01-15 09:04:54 +0800

    Initial code added.

---
+++ Assets/Scripts/Dealer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dealer
+{
+    public const int NUMBER_OF_PLAYERS = 2;
+    public const int CARDS_PER_PLAYER = 2;
+    public const int NUMBER_OF_COMMUNITY_CARDS = 5;
+
+    private Deck deck;
+
+    List<Deck.Card>[] playerHands = new List<Deck.Card>[NUMBER_OF_PLAYERS];
+    List<Deck.Card> communityCards = new List<Deck.Card>();
+
+    private Dealer()
+    {
+        deck = new Deck();
+    }
+
+    private static Dealer _instance;
+    public static Dealer GetDealer()
+    {
+        if (_instance == null)
+            _instance = new Dealer();
+
+        return _instance;
+    }
+
+    public void DealCards()
+    {
+        for(int i = 0; i < NUMBER_OF_PLAYERS; i++)
+        {
+            playerHands[i] = deck.DrawCards(CARDS_PER_PLAYER);
+        }
+    }
+
+    public void DealCommunityCards()
+    {
+        communityCards = deck.DrawCards(NUMBER_OF_COMMUNITY_CARDS);
+    }
+}
---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck
+{
+    List<Card> deckCardList = new List<Card>();
+    List<Card> dealtCards = new List<Card>();
+    List<Card> cards = new List<Card>();
+
+    public Deck()
+    {
+        CreateDeck();
+    }
+
+    private void CreateDeck()
+    {
+        deckCardList.Clear();
+
+        for (int i = 0; i < (int)Card.Suit.MAX_SUIT; i++)
+        {
+            for (int j = 0; j < (int)Card.Value.MAX_VALUE; j++)
+            {
+                deckCardList.Add(new Card((Card.Suit)i, (Card.Value)j));
+            }
+        }
+
+        Debug.Log("Deck Created! Cards: " + deckCardList.Count);
+    }
+
+    public List<Card> DrawCards(int numberOfCards)
+    {
+        cards.Clear();
+
+        for(int i = 0; i < numberOfCards; i++)
+        {
+            int cardIdx = Random.Range(0, deckCardList.Count);
+            cards.Add(deckCardList[cardIdx]);
+            dealtCards.Add(deckCardList[cardIdx]);
+            deckCardList.RemoveAt(cardIdx);
+        }
+
+        return cards;
+    }
+
+    public void ResetDeck()
+    {
+        dealtCards.Clear();
+        cards.Clear();
+
+        CreateDeck();
+    }
+
+    public struct Card
+    {
+        public enum Suit
+        {
+            HEART,
+            SPADE,
+            CLUB,
+            DIAMOND,
+            MAX_SUIT
+        }
+
+        public enum Value
+        {
+            TWO, 
+            THREE, 
+            FOUR, 
+            FIVE, 
+            SIX, 
+            SEVEN, 
+            EIGHT, 
+            NINE, 
+            TEN, 
+            JACK, 
+            QUEEN, 
+            KING, 
+            ACE,
+            MAX_VALUE
+        }
+
+        public Suit cardSuit;
+        public Value cardValue;
+
+        public Card(Suit suit, Value value)
+        {
+            cardSuit = suit;
+            cardValue = value;
+        }
+    }
+}

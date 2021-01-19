b114c18f02898843a01b33737310f8b83f6f9d69
aldrich-pico <62074615+aldrich-pico@users.noreply.github.com>
2021-01-19 09:45:56 +0800

    * Added animations to the game

--- Assets/Scripts/Dealer.cs
+++ Assets/Scripts/Dealer.cs
@@ -46,17 +46,6 @@ public class Dealer
             playerHands[i].AddRange(deck.DrawCards(CARDS_PER_PLAYER));
         }
 
-        /*DEBUG
-         {
-            Debug.Log("Player 1:");
-            Debug.Log(playerHands[0][0].cardValue.ToString()+"--"+ playerHands[0][0].cardSuit.ToString());
-            Debug.Log(playerHands[0][1].cardValue.ToString() + "--" + playerHands[0][1].cardSuit.ToString());
-
-            Debug.Log("Player 2:");
-            Debug.Log(playerHands[1][0].cardValue.ToString() + "--" + playerHands[1][0].cardSuit.ToString());
-            Debug.Log(playerHands[1][1].cardValue.ToString() + "--" + playerHands[1][1].cardSuit.ToString());
-        }
-         //*/
         return playerHands;
     }
 
@@ -69,60 +58,11 @@ public class Dealer
 
         communityCards.AddRange(deck.DrawCards(NUMBER_OF_COMMUNITY_CARDS));
 
-        /*DEBUG
-        {
-            Debug.Log("Community:");
-            Debug.Log(communityCards[0].cardValue.ToString() + "--" + communityCards[0].cardSuit.ToString());
-            Debug.Log(communityCards[1].cardValue.ToString() + "--" + communityCards[1].cardSuit.ToString());
-            Debug.Log(communityCards[2].cardValue.ToString() + "--" + communityCards[2].cardSuit.ToString());
-            Debug.Log(communityCards[3].cardValue.ToString() + "--" + communityCards[3].cardSuit.ToString());
-            Debug.Log(communityCards[4].cardValue.ToString() + "--" + communityCards[4].cardSuit.ToString());
-        }
-        //*/
         return communityCards;
     }
 
     public void EvaluateHands()
     {
-        /*
-        //DEBUG
-        {
-            playerHands[0].Clear();
-            playerHands[1].Clear();
-            communityCards.Clear();
-
-            playerHands[0].Add(new Card(Card.Suit.CLUB, Card.Value.THREE));
-            playerHands[0].Add(new Card(Card.Suit.SPADE, Card.Value.SIX));
-
-            playerHands[1].Add(new Card(Card.Suit.HEART, Card.Value.SIX));
-            playerHands[1].Add(new Card(Card.Suit.SPADE, Card.Value.SIX));
-
-            communityCards.Add(new Card(Card.Suit.SPADE, Card.Value.THREE));
-            communityCards.Add(new Card(Card.Suit.DIAMOND, Card.Value.THREE));
-            communityCards.Add(new Card(Card.Suit.DIAMOND, Card.Value.SIX));
-            communityCards.Add(new Card(Card.Suit.DIAMOND, Card.Value.FIVE));
-            communityCards.Add(new Card(Card.Suit.HEART, Card.Value.QUEEN));
-        {
-            Debug.Log("Player 1:");
-            Debug.Log(playerHands[0][0].cardValue.ToString()+"--"+ playerHands[0][0].cardSuit.ToString());
-            Debug.Log(playerHands[0][1].cardValue.ToString() + "--" + playerHands[0][1].cardSuit.ToString());
-
-            Debug.Log("Player 2:");
-            Debug.Log(playerHands[1][0].cardValue.ToString() + "--" + playerHands[1][0].cardSuit.ToString());
-            Debug.Log(playerHands[1][1].cardValue.ToString() + "--" + playerHands[1][1].cardSuit.ToString());
-        }
-
-        {
-            Debug.Log("Community:");
-            Debug.Log(communityCards[0].cardValue.ToString() + "--" + communityCards[0].cardSuit.ToString());
-            Debug.Log(communityCards[1].cardValue.ToString() + "--" + communityCards[1].cardSuit.ToString());
-            Debug.Log(communityCards[2].cardValue.ToString() + "--" + communityCards[2].cardSuit.ToString());
-            Debug.Log(communityCards[3].cardValue.ToString() + "--" + communityCards[3].cardSuit.ToString());
-            Debug.Log(communityCards[4].cardValue.ToString() + "--" + communityCards[4].cardSuit.ToString());
-        }
-        }
-        //*/
-
         for (int i = 0; i < NUMBER_OF_PLAYERS; i++)
         {
             List<Card> combinedCards = new List<Card>();
---
+++ Assets/Scripts/GameFunctionCaller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFunctionCaller : MonoBehaviour
+{
+    [SerializeField]
+    private Game game;
+    
+    public void CallStartCommunityCardAnimation()
+    {
+        game.StartCommunityCardsAnimation();
+    }
+
+    public void CallStartPlayerCardAnimation()
+    {
+        game.StartPlayerCardsAnimation();
+    }
+
+    public void CallStartEvaluation()
+    {
+        game.StartEvaluation();
+    }
+}
